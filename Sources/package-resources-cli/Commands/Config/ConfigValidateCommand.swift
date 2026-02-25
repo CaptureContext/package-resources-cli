22a2c739bb167480a35f95f2e9555406298c1aee
maximkrouk <id.maximkrouk@gmail.com>
2026-02-25 02:04:58 +0100

    feat: Add config validate command

---
+++ Sources/package-resources-cli/Commands/Config/ConfigValidateCommand.swift
@@ -0,0 +1,34 @@
+import ArgumentParser
+import Foundation
+import PackageResourcesFS
+import Yams
+
+extension App.ConfigCommand {
+	struct ValidateCommand: ParsableCommand {
+		static let configuration: CommandConfiguration = .init(
+			commandName: "validate",
+			abstract: "Validates configuration file",
+			subcommands: []
+		)
+
+		@ParentCommand
+		var parent: App.ConfigCommand
+
+		func run() throws {
+			let configFile = File(uncheckedPath: parent.path)
+			let missing = (try? configFile.validatePath()) == nil
+
+			if missing {
+				throw _Error("⚠️ \(configFile.name) file is missing at \(parent.path) path")
+			}
+
+			_ = try Manifest.load(at: configFile.path)
+
+			print(
+				ANSI("✅ Successfully validated \(configFile.name) file")
+					.foreground(.green)
+					.bold()
+			)
+		}
+	}
+}
