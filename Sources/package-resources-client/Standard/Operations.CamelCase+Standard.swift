45d7f800fbff6c1001def95742fb896d7c363935
maximkrouk <id.maximkrouk@gmail.com>
2026-02-25 02:04:44 +0100

    feat: Update camelCase format

--- Sources/package-resources-client/Standard/Operations.CamelCase+Standard.swift
+++ Sources/package-resources-client/Standard/Operations.CamelCase+Standard.swift
@@ -2,7 +2,7 @@ import Casification
 
 extension PackageResourcesClient.Operations.CamelCase {
 	public static func standard(
-		_ mode: String.Casification.Configuration.CamelCase.Mode = .automatic
+		_ mode: String.Casification.Configuration.CamelCase.Mode = .camel
 	) -> Self {
 		return .init {
 			$0.case(.camel(mode))
