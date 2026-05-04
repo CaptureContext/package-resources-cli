1049981a5aa4375399d82a0a136e27d16bb8e7a7
maximkrouk <id.maximkrouk@gmail.com>
2026-05-05 01:03:42 +0200

    fix: Adjust platform versions

--- Package.swift
+++ Package.swift
@@ -5,11 +5,7 @@ import PackageDescription
 let package = Package(
 	name: "package-resources-cli",
 	platforms: [
-		.iOS(.v13),
-		.macOS(.v10_15),
-		.tvOS(.v13),
-		.watchOS(.v6),
-		.macCatalyst(.v13),
+		.macOS(.v13),
 	],
 	products: [
 		.plugin(
