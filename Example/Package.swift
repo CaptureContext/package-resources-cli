d84ac396230e4992a34a0d58ceed8267d1aab9b5
maximkrouk <id.maximkrouk@gmail.com>
2026-09-03 11:29:09 +0200

    chore: remove local example package overrides

--- Example/Package.swift
+++ Example/Package.swift
@@ -25,15 +25,15 @@ let package = Package(
 	dependencies: [
 		.package(
 			url: "https://github.com/capturecontext/package-resources-cli.git",
-			from: "3.0.0"
+			.upToNextMajor(from: "5.0.0")
 		),
 		.package(
 			url: "https://github.com/capturecontext/swift-package-resources.git",
-			from: "4.0.0"
+			.upToNextMajor(from: "5.0.1")
 		),
 		.package(
 			url: "https://github.com/capturecontext/cocoa-aliases.git",
-			from: "3.3.0"
+			.upToNextMajor(from: "3.5.2")
 		),
 	],
 	targets: [
@@ -83,40 +83,3 @@ let package = Package(
 	],
 	swiftLanguageModes: [.v6]
 )
-
-extension Array<Package.Dependency> {
-	/// Use this to depend on local cli
-	func useLocalPackageResourcesCLI() -> Self {
-		_override("package-resources-cli")
-	}
-
-	/// CaptureContext internal alias for local development
-	func useLocalPackageResources() -> Self {
-		_override("swift-package-resources")
-	}
-
-	/// CaptureContext internal alias for local development
-	func useLocalCasification() -> Self {
-		_override("swift-casification")
-	}
-
-	/// CaptureContext internal alias for local development
-	func cctx_local_development() -> Self {
-		self.useLocalCasification()
-			.useLocalPackageResources()
-			.useLocalPackageResourcesCLI()
-	}
-
-	private func _override(_ name: String) -> Self {
-		[.package(path: "../../\(name)")] + self.filter {
-			switch $0.kind {
-			case let .fileSystem(packageName, path):
-				return name != packageName && !path.contains(name)
-			case let .sourceControl(packageName, location, _):
-				return name != packageName && !location.contains(name)
-			default:
-				return true
-			}
-		}
-	}
-}
